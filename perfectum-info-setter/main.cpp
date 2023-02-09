eb4adea6953f065c0eed3e18d19e4b074f70e730
directx14 <ak9734t10@gmail.com>
2023-02-09 17:20:32 +0100

    Second task done
    Added switching Computer Name

--- perfectum-info-setter/main.cpp
+++ perfectum-info-setter/main.cpp
@@ -5,12 +5,23 @@
 using namespace std;
 
 bool setWallpaper(string);
+bool setComputerName(string);
+
+bool setRegistryOEMInfo(string, string);
 
 int main() {
     cout << "Script started..." << endl;
 
     const string wallPic = "Perfectum_Background";
     bool status = setWallpaper(wallPic);
+    cout << "Status: " << status << endl;
+
+    status = setComputerName("Perfectum-PC");
+    cout << "Status: " << status << endl;
+
+
+    cout << "Editing registry..." << endl;
+    status << setRegistryOEMInfo("asd", "asdd");
     cout << "Status: " << status << endl;
 
 	return 0;
@@ -29,4 +40,20 @@ bool setWallpaper(string imageName)
     const wchar_t* filenm = widestr.c_str();
     // Set the wallpaper to the given image
     return SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (void*)filenm, SPIF_UPDATEINIFILE);
+}
+
+bool setComputerName(string computerName) {
+    cout << "Changing PC Name..." << endl;
+
+    DWORD dwLastErr;
+
+    bool flResult = SetComputerNameExA(ComputerNamePhysicalDnsHostname, computerName.c_str());
+
+    if (flResult == 0)
+    {
+        dwLastErr = GetLastError();
+        wcout << dwLastErr << endl;
+    }
+
+    return flResult;
 }
